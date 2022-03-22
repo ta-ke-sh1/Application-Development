1485369cf01f7f83ccf3c37043902aa769bed2e0
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2022-03-22 09:12:03 +0700

    login

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,15 +1,46 @@
 const express = require("express");
 const router = express.Router();
-const { insertObject, getAll, deleteObject, getObject } = require("../databaseHandler");
+const {
+    insertObject,
+    getAll,
+    deleteObject,
+    getObject,
+    checkUser,
+} = require("../databaseHandler");
 
 //neu request la: /admin
 router.get("/", async (req, res) => {
-    res.render("Admin/adminIndex", { books: await getAll("Books"), title: "Admin" });
+    res.render("Admin/adminIndex", {
+        books: await getAll("Books"),
+        title: "Admin",
+    });
 });
 
 //neu request la: /admin/addUser
 router.get("/addUser", (req, res) => {
     res.render("Admin/addUser");
+});
+
+router.get("/register", (req, res) => {
+    res.render("Admin/register");
+});
+
+router.post("/register", (req, res) => {
+    const name = req.body.txtName;
+    const pass = req.body.txtPassword;
+    if (!getUser(name)) {
+        const objectToInsert = {
+            userName: name,
+            role: "Customer",
+            password: pass,
+        };
+        insertObject("Users", objectToInsert);
+        res.redirect("/login");
+    } else {
+        res.render("/Admin/register", {
+            error: "Existing user!",
+        });
+    }
 });
 
 //Submit add User
@@ -37,25 +68,44 @@ router.post("/addNewBook", (req, res) => {
     const path = __dirname + "/../public/Books/" + image.name;
     image.mv(path, (err) => {
         if (err) throw err;
-    })
+    });
     const objectToInsert = {
         name: name,
         description: description,
         price: price,
         quantity: quantity,
-        image: image.name
-    }
+        image: image.name,
+    };
     insertObject("Books", objectToInsert);
-    res.redirect("/admin/")
-})
+    res.redirect("/admin/");
+});
 // Add Book Render
 router.get("/addBook", (req, res) => {
     res.render("Admin/addBook");
-})
+});
 
 router.get("/deleteBook/:id", async (req, res) => {
     await deleteObject("Books", getObject(req.params.id, "Books"));
     res.redirect("/");
-})
+});
+
+router.get("/login", async (req, res) => {
+    res.render("login");
+});
+
+// Kiem tra thong tin dang nhap
+router.post("/login", async (req, res) => {
+    const name = req.body.txtUsername;
+    const password = req.body.txtPassword;
+    var role = await checkUser(name, password);
+    if (role == "-1") {
+        res.end("Invalid User!");
+    } else {
+        res.end("You are " + role);
+        // res.render('/login', {
+        //     error: "Wrong password or username!"
+        // });
+    }
+});
 
 module.exports = router;
