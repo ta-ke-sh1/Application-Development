9de023afbb943d2e9d5fa42e29041e1a09402268
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2022-04-05 10:34:36 +0700

    user routing

--- controllers/user.js
+++ controllers/user.js
@@ -5,10 +5,32 @@ const { getUser } = require("../databaseHandler");
 const session = require("express-session");
 
 router.get("/edit", (req, res) => {
+    res.render("User/edit", {});
+});
+
+router.get("/cart", (req, res) => {
     console.log(req.session);
-    res.render("User/edit", {
-        output: req.session,
-    });
+    res.render("User/cart", {});
+});
+
+router.get("/checkout", (req, res) => {
+    console.log(req.session);
+    res.render("User/checkout", {});
+});
+
+router.get("/edit", (req, res) => {
+    console.log(req.session);
+    res.render("User/edit", {});
+});
+
+router.get("/profile", (req, res) => {
+    console.log(req.session);
+    res.render("User/profile", {});
+});
+
+router.get("/feedback", (req, res) => {
+    console.log(req.session);
+    res.render("User/edit", {});
 });
 
 router.post("/edit", (req, res) => {
