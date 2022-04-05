232a36e9189e20d7cdf41887d1111db74a95c239
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2022-04-05 10:36:00 +0700

    user access control

--- controllers/user.js
+++ controllers/user.js
@@ -4,36 +4,48 @@ const router = express.Router();
 const { getUser } = require("../databaseHandler");
 const session = require("express-session");
 
-router.get("/edit", (req, res) => {
+// access control
+function requiresLogin(req, res, next) {
+    if (req.session) {
+        return next();
+    } else {
+        var err = "You must be have logged in to view this page.";
+        res.render("login", {
+            error: err,
+        });
+    }
+}
+
+router.get("/edit", requiresLogin, (req, res) => {
     res.render("User/edit", {});
 });
 
-router.get("/cart", (req, res) => {
+router.get("/cart", requiresLogin, (req, res) => {
     console.log(req.session);
     res.render("User/cart", {});
 });
 
-router.get("/checkout", (req, res) => {
+router.get("/checkout", requiresLogin, (req, res) => {
     console.log(req.session);
     res.render("User/checkout", {});
 });
 
-router.get("/edit", (req, res) => {
+router.get("/edit", requiresLogin, (req, res) => {
     console.log(req.session);
     res.render("User/edit", {});
 });
 
-router.get("/profile", (req, res) => {
+router.get("/profile", requiresLogin, (req, res) => {
     console.log(req.session);
     res.render("User/profile", {});
 });
 
-router.get("/feedback", (req, res) => {
+router.get("/feedback", requiresLogin, (req, res) => {
     console.log(req.session);
     res.render("User/edit", {});
 });
 
-router.post("/edit", (req, res) => {
+router.post("/edit", requiresLogin, (req, res) => {
     const pass = txt.body.txtPassword;
     const user = getUser(req.session["User"]);
     if (user.password != pass) {
