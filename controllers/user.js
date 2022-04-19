e6e2150928bfb43caa45599be2259d35c2e7056d
Applexive <90685277+Applexive@users.noreply.github.com>
2022-04-19 08:51:54 +0700

    update profile

--- controllers/user.js
+++ controllers/user.js
@@ -4,11 +4,18 @@ const router = express.Router();
 const {
     getUser,
     getObject,
+    createCipher,
+    checkUser,
     insertObject,
+    updateObject,
     getAllObject,
 } = require("../databaseHandler");
 const session = require("express-session");
 const async = require("hbs/lib/async");
+const crypto = require("crypto");
+const algorithm = "aes-256-cbc";
+const Securitykey = "trungha";
+
 
 function requiresLogin(req, res, next) {
     if (req.session) {
@@ -73,8 +80,11 @@ router.post("/checkout", requiresLogin, (req, res) => {
     res.render("User/checkout", {});
 });
 
-router.get("/edit", requiresLogin, (req, res) => {
-    res.render("User/edit", {});
+router.get("/edit", requiresLogin, async (req, res) => {
+    const objectToUpdate = await getObject(req.session.userID, "Users");
+    res.render("User/edit", {
+        userProf: objectToUpdate,
+    });
 });
 
 router.get("/profile", requiresLogin, (req, res) => {
@@ -85,10 +95,55 @@ router.get("/feedback", requiresLogin, (req, res) => {
     res.render("User/edit", {});
 });
 
-router.post("/edit", requiresLogin, (req, res) => {
-    const oldPassword = txt.body.txtOldPassword;
-    const user = checkUser(req.session["User"], oldPassword);
-    if (user != "-1") {} else {}
+router.post("/edit", requiresLogin, async (req, res) => {
+    const fname = req.body.txtFirstName;
+    const lname = req.body.txtLastName;
+    const address = req.body.txtAddress;
+    const phone = req.body.telPhone;
+    const pass = req.body.txtPassword;
+    const oldPass = req.body.txtOldPassword;
+
+    const objectToUpdate = await getObject(req.session.userID, "Users");
+    if (encrypt(oldPass) == objectToUpdate.password) {
+        if (req.files != null) {
+            const avatar = req.files.avatar;
+            avatar.name = fname + uniqid() + ".jpg";
+            const path = __dirname + "/../public/Avatars/" + avatar.name;
+            avatar.mv(path, (err) => {
+                if (err) throw err;
+            });
+
+            var updateValues = {
+                $set: {
+                    firstName: fname,
+                    lastName: lname,
+                    address: address,
+                    phoneNumber: phone,
+                    avatar: avatar.name,
+                    password: encrypt(pass),
+                },
+            };
+            
+        } else {
+            var updateValues = {
+                $set: {
+                    firstName: fname,
+                    lastName: lname,
+                    address: address,
+                    phoneNumber: phone,
+                    password: encrypt(pass),
+                },
+            };
+            
+        }
+        await updateObject("Users", objectToUpdate, updateValues);
+        res.redirect("/user/profile");
+    } else {
+        res.render("User/edit", {
+            userProf: objectToUpdate,
+            error: "Incorrect old password"
+        })
+    }
 });
 
 router.get("/orders", async(req, res) => {
@@ -116,5 +171,12 @@ router.post("/addFeedback", async(req, res) => {
     await insertObject("Feedbacks", Feedback);
     res.redirect("/orders");
 });
+
+function encrypt(text) {
+    let encrypted = crypto.createCipher(algorithm, Securitykey);
+    let result = encrypted.update(text, "utf8", "hex");
+    result += encrypted.final("hex");
+    return result;
+}
 
 module.exports = router;
